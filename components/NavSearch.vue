5dbe9a5cb5fe50c83e9f23251a7eccfad72feee2
George Deeks <george@neontribe.co.uk>
2020-07-30 15:15:26 +0100

    fix: searchbar length and no results

--- components/NavSearch.vue
+++ components/NavSearch.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="navbar-item custom-width">
-    <div class="control has-addons custom-width">
+    <div class="control has-addons custom-width-child">
       <b-autocomplete
+        open-on-focus
         rounded
         v-model="query"
         :data="results"
-        placeholder="Search or jump to.."
-        field="title"
+        placeholder="Search or jump to..."
         :loading="isFetching"
         @typing="getAsyncData"
         custom-class="can-expand"
@@ -15,13 +15,16 @@
       >
         <template slot="header">
           <div class="truncate">
-            <nuxt-link :to="{ name: 'search', query: { query: query }}">
+            <nuxt-link :to="{ name: 'search', query: { query: query }}" v-if="query.length">
               Search whole site for
               <strong>{{shortText}}</strong>
             </nuxt-link>
           </div>
         </template>
-        <template slot="empty">No results for {{query}}</template>
+        <template slot="empty">
+          <div v-if="query.length">No results for {{query}}</div>
+          <div v-else>Type in the search bar to see results</div>
+        </template>
         <template slot-scope="props">
           <div class="truncate" v-if="props.option.type=='project'">
             <b-icon icon="folder-text-outline" size="is-small" class="has-text-grey"></b-icon>
@@ -67,6 +70,13 @@ export default {
         query: { id: item._id }
       });
     },
+    // TODO can't get this to work
+    // @keyup.enter="searchEntireSite"
+    // searchEntireSite: function() {
+    //   if (query){
+    //     this.$router.push({ path: 'search', query: { query: query } })
+    //   }
+    // },
     getAsyncData: debounce(function(query) {
       this.isFetching = true;
       this.results.length = 0;
@@ -74,21 +84,24 @@ export default {
         .get("/search", { params: { query: query } })
         .then(res => {
           this.isFetching = false;
+
+          const { projects, samples, runs } = res.data.results;
+
           //TODO sort data
-          if (res.data.results.project) {
-            res.data.results.projects.map(p => {
+          if (projects) {
+            projects.map(p => {
               p.type = "project";
               this.results.push(p);
             });
           }
-          if (res.data.results.samples) {
-            res.data.results.samples.map(p => {
+          if (samples) {
+            samples.map(p => {
               p.type = "sample";
               this.results.push(p);
             });
           }
-          if (res.data.results.runs) {
-            res.data.results.runs.map(p => {
+          if (runs) {
+            runs.map(p => {
               p.type = "run";
               this.results.push(p);
             });
@@ -101,15 +114,21 @@ export default {
         });
 
       // this.data = this.$store.getters.filteredProjects(query);
-    }, 500)
+    }, 800)
   }
 };
 </script>
 
 <style scoped>
+
 .custom-width {
   width: 100%;
   max-width: 500px;
+}
+
+.custom-width-child {
+  width: 100%;
+  max-width: 499px;
 }
 
 </style>
