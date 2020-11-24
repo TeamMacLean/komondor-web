cb357a2f11f2df98bac5746af87c38ecefd8d6e3
George Deeks <george@neontribe.co.uk>
2020-11-24 16:30:56 +0000

    fix: autofill cbox for new forms TEMP

--- components/formHelpers/FormConsentCheckbox.vue
+++ components/formHelpers/FormConsentCheckbox.vue
@@ -1,14 +1,22 @@
 <template>
     <div class="field">
-        <b-checkbox>
+        <b-checkbox v-model="checkbox">
+            <!-- TODO remove checked attribute -->
             <!-- TODO add required to b-checkbox -->
-        I confirm that I have carefully checked all of my input data for this form, and that it is in its final state.
-        I accept that, once submitted, I may not be able to edit these fields, including their submission to ENA.
+            I confirm that I have carefully checked all of my input data for this form, and that it is in its final state.
+            I accept that, once submitted, I may not be able to edit these fields, including their submission to ENA.
         </b-checkbox>
     </div>
 </template>
 
 <script>
+    export default {
+        data() {
+            return {
+                checkbox: true,
+            }
+        }
+    }
 </script>
 
 <style>
