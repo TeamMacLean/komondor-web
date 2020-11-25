e6743bf58f6727b1a7145d1055339900682686e0
George Deeks <george@neontribe.co.uk>
2020-11-25 21:16:02 +0000

    fix: checkbox now reports state to parent

--- components/formHelpers/FormConsentCheckbox.vue
+++ components/formHelpers/FormConsentCheckbox.vue
@@ -1,19 +1,27 @@
 <template>
-    <div class="field">
-        <b-checkbox v-model="checkbox">
-            <!-- TODO remove checked attribute -->
-            <!-- TODO add required to b-checkbox -->
+    <div class="field" @click=check()>
+        <b-checkbox type="checkbox" v-model="checked" required>
             I confirm that I have carefully checked all of my input data for this form, and that it is in its final state.
-            I accept that, once submitted, I may not be able to edit these fields, including their submission to ENA.
+            I accept that, once submitted, I may not be able to edit these fields, including their submission to ENA.    
         </b-checkbox>
     </div>
 </template>
 
 <script>
     export default {
+        props: ['initialVal', 'onToggle'],
         data() {
             return {
-                checkbox: true,
+                checked: !!this.initialVal, // defaults to false if no value supplied
+            }
+        },
+        methods: {
+            check: function() {
+                // report the opposite of the pre-toggled state
+                const newState = !this.checked;
+                if (!!this.onToggle){
+                    this.onToggle(newState)
+                }
             }
         }
     }
@@ -21,4 +29,3 @@
 
 <style>
 </style>
-
