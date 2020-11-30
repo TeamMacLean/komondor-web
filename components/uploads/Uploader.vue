144d218a8321f2b65bd9ddd52aec6edbeb0a8fa6
George Deeks <george@neontribe.co.uk>
2020-11-30 16:03:51 +0000

    fix: access control allow credentials false

--- components/uploads/Uploader.vue
+++ components/uploads/Uploader.vue
@@ -61,12 +61,12 @@ export default {
         endpoint: this.API_URL + "/uploads",
         resume: true,
         limit: 10,
-        // headers: {
-        //   'Access-Control-Allow-Origin': '*',
-        //   'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-        //   'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Authorization, Origin, Accept',
-        //   'Access-Control-Allow-Credentials': true,
-        // },
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+          'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Authorization, Origin, Accept',
+          'Access-Control-Allow-Credentials': false, // false cos of line 61
+        },
         // I THINK THIS IS THE KING
         overridePatchMethod: true,
         // removeFingerprintOnSuccess: true; means a new upload if same file is uploaded again
