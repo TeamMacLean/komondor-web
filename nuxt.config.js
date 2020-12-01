34cd3905a46517b12291ecb16a3939f5cac470f0
George Deeks <george@neontribe.co.uk>
2020-12-01 12:28:22 +0000

    fix: access control allow origin only

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,9 +9,9 @@ export default {
     // global, so try to avoid
     headers: {
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, HEAD, POST, OPTIONS, PUT, PATCH, DELETE',
-      'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Authorization, Origin, Accept',
-      'Access-Control-Allow-Credentials': false,
+      // 'Access-Control-Allow-Methods': 'GET, HEAD, POST, OPTIONS, PUT, PATCH, DELETE',
+      // 'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Authorization, Origin, Accept',
+      // 'Access-Control-Allow-Credentials': false,
     }
   },
   env: {
